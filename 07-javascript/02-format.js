5bda6a069b144e0567dbb482dba33c910a494e61
Kevin Macdonald <kjm25@cecs.pdx.edu>
2024-11-10 18:54:30 -0800

    07-js started

--- 07-javascript/02-format.js
+++ 07-javascript/02-format.js
@@ -4,6 +4,9 @@ const formatItems = (input) => {
   // for reference, see the MDN Number page:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
 
+  input = input.map((ele) => ele.toFixed(3));
+  input = input.join(", ");
+
   return input;
 };
 
